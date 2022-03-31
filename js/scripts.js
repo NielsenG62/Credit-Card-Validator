643d8cb075e70f55b894f89fce7c060edcadbd75
Gabe Nielsen <gabenielsen7@gmail.com>
2022-03-31 13:17:18 -0700

    WIP: adding double digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 function luhnDouble(input) {
-  const card = input.split('')
+  const card = input.split("");
   const doubled = [];
   for (i = 0; i < card.length; i++) {
     if (i % 2 === 0) {
-      doubled.push(card[i] * 1)
-    }
-    else {
-      doubled.push(card[i] * 2)
+      doubled.push(card[i] * 1);
+    } else {
+      doubled.push(card[i] * 2);
     }
   }
   return doubled;
 }
 
 function luhnAddDouble(input) {
-  let doubled = luhnDouble(input)
+  let doubled = luhnDouble(input);
   for (i = 0; i < doubled.length; i++) {
     if (doubled[i] >= 10) {
-      let splitDouble = doubled[i].toString().split('');
-      let firstDigit = number(splitDouble[0])
-      let secondDigit = number(splitDouble[1])
+      let splitDouble = doubled[i].toString().split("");
+      let firstDigit = parseFloat(splitDouble[0]);
+      let secondDigit = parseFloat(splitDouble[1]);
+      let newNum = firstDigit + secondDigit;
+      // doubled.splice(i, newNum)
+      debugger;
+      return doubled;
     }
   }
 }
-
-
-
